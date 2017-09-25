9e9c616b509a2cbd313b5a4431e239d05a439503
twchuck1 <32159827+twchuck@users.noreply.github.com>
2017-09-25 10:35:28 +0800

    init java source file

---
+++ src/main/Calculator.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Calculator {
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+	public static int sub(int a, int b) {
+		return a - b;
+	}
+
+	public static int multiply(int a, int b) {
+		return a * b;
+	}
+}
