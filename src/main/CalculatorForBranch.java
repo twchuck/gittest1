d82989b5be7d0ee05020f3120ab3fb374c752f2e
twchuck1 <32159827+twchuck@users.noreply.github.com>
2017-09-25 14:39:32 +0800

    init branch file

---
+++ src/main/CalculatorForBranch.java
@@ -0,0 +1,16 @@
+package main;
+
+public class CalculatorForBranch {
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+	public static int sub(int a, int b) {
+		return a - b;
+	}
+
+	public static int multiply(int a, int b) {
+		return a * b;
+	}
+}
