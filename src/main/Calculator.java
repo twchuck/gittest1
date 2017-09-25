82a8825ad260d70cec1dbbd3bfb32d91744600ee
twchuck1 <32159827+twchuck@users.noreply.github.com>
2017-09-25 15:05:12 +0800

    branch modify multiply for 2 b

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -11,9 +11,9 @@ public class Calculator {
 	}
 
 	public static int multiply(int a, int b) {
-		return a * b;
+		return a * b * b;
 	}
-	
+
 	public static int div(int a, int b) {
 		return a / b;
 	}
